d8a4d20e739d902b443a545341c1648e0741bf42
Beatriz de Oliveira Barros <bob@windows.cin.ufpe.br>
2024-03-07 14:46:41 -0300

    update user data frontend concluido

--- frontend/src/app/home/pages/UserUpdate/index.tsx
+++ frontend/src/app/home/pages/UserUpdate/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styles from './UserUpdateform.module.css';
 
 interface UpdateFormData {
   oldEmail: string;
@@ -43,31 +44,38 @@ const UpdateForm: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Update User Data</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Old Email:</label>
-          <input type="email" name="oldEmail" value={formData.oldEmail} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>New Name:</label>
-          <input type="text" name="newName" value={formData.newName} onChange={handleChange} />
-        </div>
-        <div>
-          <label>New Email:</label>
-          <input type="email" name="newEmail" value={formData.newEmail} onChange={handleChange} />
-        </div>
-        <div>
-          <label>New Password:</label>
-          <input type="password" name="newPassword" value={formData.newPassword} onChange={handleChange} />
-        </div>
-        <div>
-          <label>New Username:</label>
-          <input type="text" name="newUserName" value={formData.newUserName} onChange={handleChange} />
-        </div>
-        <button type="submit">Update</button>
-      </form>
+    <div className={styles.container}>
+      <div className={styles.UpdateUser}>
+        <h2>Update User Data</h2>
+      </div>
+      <div className={styles.EnterYourInfo}>
+        <h5>Enter your email below to login to your account</h5>
+      </div>
+      <div className={styles.fieldContainer}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.inputContainer}>
+            <label htmlFor="oldEmail" className={styles.inputLabel}>Old Email:</label>
+            <input type="email" id="oldEmail" name="oldEmail" placeholder="Old Email" value={formData.oldEmail} onChange={handleChange} required className={styles.input} />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="newName" className={styles.inputLabel}>New Name:</label>
+            <input type="text" id="newName" name="newName" placeholder="New Name" value={formData.newName} onChange={handleChange} className={styles.input} />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="newEmail" className={styles.inputLabel}>New Email:</label>
+            <input type="email" id="newEmail" name="newEmail" placeholder="New Email" value={formData.newEmail} onChange={handleChange} className={styles.input} />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="newPassword" className={styles.inputLabel}>New Password:</label>
+            <input type="password" id="newPassword" name="newPassword" placeholder="New Password" value={formData.newPassword} onChange={handleChange} className={styles.input} />
+          </div>
+          <div className={styles.inputContainer}>
+            <label htmlFor="newUserName" className={styles.inputLabel}>New Username:</label>
+            <input type="text" id="newUserName" name="newUserName" placeholder="New Username" value={formData.newUserName} onChange={handleChange} className={styles.input} />
+          </div>
+          <button type="submit" className={`${styles.button} ${styles.updateButton}`}>Update</button>
+        </form>
+      </div>
       <div>{updateMessage}</div>
     </div>
   );
